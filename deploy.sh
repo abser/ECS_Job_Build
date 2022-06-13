eb893dc78715c22f4b67e1e062d21923eaed725a
Nurul Abser <abser.it@gmail.com>
2022-06-13 21:36:58 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ REGION="ap-southeast-1"
 #FAMILY=`sed -n 's/.*"family": "\(.*\)",/\1/p' task_def.json`
 FAMILY="test-app"
 echo $USER
-Docker=`aws ecr get-login-password --region $REGION --profile $AWS_PROFILE`
+Docker=`aws ecr get-login-password --region $REGION`
 echo "Logging into target ECR ($AWS_PROFILE_TARGET)" && \
 docker login -u AWS -p ${Docker} https://${ECR_URL_TARGET} && \
 
@@ -31,7 +31,7 @@ docker push ${ECR_URL_TARGET}:${VERSION_NUMBER}
 sed -i "s+{{DOCKER_IMAGE_URL}}+$ECR_URL_TARGET:$VERSION_NUMBER+g" ./task_def.json
 
 # Add the new task defination
-NEW_TASK_DEFINITION=$(aws ecs register-task-definition --cli-input-json file://./task_def.json --region $REGION --profile $AWS_PROFILE) && \
+NEW_TASK_DEFINITION=$(aws ecs register-task-definition --cli-input-json file://./task_def.json --region $REGION) && \
 
 # Get new task defination ARN
 NEW_TASK_ARN=$(echo "$NEW_TASK_DEFINITION" | jq '.taskDefinition.taskDefinitionArn' | sed -e 's/^"//' -e 's/"$//') && \
@@ -43,20 +43,20 @@ then
   exit 1
 fi
 
-SERVICES=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER_NAME} --region ${REGION} --profile ${AWS_PROFILE} | jq .failures[]`
+SERVICES=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER_NAME} --region ${REGION} | jq .failures[]`
 echo "SERVICES: $SERVICES"
 #Get latest revision
-REVISION=`aws ecs describe-task-definition --task-definition ${NEW_TASK_ARN} --region ${REGION} --profile ${AWS_PROFILE} | jq .taskDefinition.revision`
+REVISION=`aws ecs describe-task-definition --task-definition ${NEW_TASK_ARN} --region ${REGION} | jq .taskDefinition.revision`
 echo "REVISION: $REVISION"
 #Create or update service
 if [ "$SERVICES" == "" ]; then
   echo "entered existing service"
-  DESIRED_COUNT=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER_NAME} --region ${REGION} --profile ${AWS_PROFILE} | jq .services[].desiredCount`
+  DESIRED_COUNT=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER_NAME} --region ${REGION} | jq .services[].desiredCount`
   if [ "${DESIRED_COUNT}" == "0" ]; then
     DESIRED_COUNT="1"
   fi
   aws ecs update-service --region ${REGION} --cli-input-json file://./update_service.json
 else
   echo "entered new service"
-  aws ecs create-service --cli-input-json file://./create_service.json  --region ${REGION} --profile ${AWS_PROFILE}
+  aws ecs create-service --cli-input-json file://./create_service.json  --region ${REGION} 
 fi
